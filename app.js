6deb846a90bc97a70ba5ef17c695541ba067b48e
Amanda Faulkenberry <amandalfwrites@gmail.com>
2022-07-16 14:14:44 -0400

    fixed typo

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 
-let transporter = nodemailer.crearTransport(transport[, defaults])
+let transporter = nodemailer.createTransport(transport[, defaults])
 let receiver = document.querySelector('#fname').value + document.querySelector('#lname').value
-
+let receiverEmail = document.querySelector('#email').value
 
 
 let response = {
     from: "sender@email.com",
-    to: 
+    to: receiverEmail,
+    subject: "Thanks for Reaching Out!",
+    text: 
 }
