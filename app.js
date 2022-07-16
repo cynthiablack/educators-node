20a83ce8df82f23f01eeba8c578cbd8f62e2d00f
Amanda Faulkenberry <amandalfwrites@gmail.com>
2022-07-16 14:56:22 -0400

    added function emailRes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.querySelector('button').addEventListener('click', emailRes)
 let receiver = document.querySelector('#fname').value + document.querySelector('#lname').value
 let receiverEmail = document.querySelector('#email').value
 
+function emailRes() {
     
     let transporter = nodemailer.createTransport({
         sendmail: true,
@@ -18,4 +19,6 @@ let receiverEmail = document.querySelector('#email').value
     subject: "Thanks for Reaching Out!",
     text: "We have received your message and will reach out to you with futher information soon."
     })
+
+    }
 
