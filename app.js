16ff25616d236c60c782707a5b71fa4677d29897
Amanda Faulkenberry <amandalfwrites@gmail.com>
2022-07-16 14:07:57 -0400

    worked on repsonse object

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 
 let transporter = nodemailer.crearTransport(transport[, defaults])
-let receiver = document.querySelector('#fname').
+let receiver = document.querySelector('#fname').value + document.querySelector('#lname').value
+
+
 
 let response = {
     from: "sender@email.com",
