94561b8cfe892bfedb780c992792d7e50f3d8284
Amanda Faulkenberry <amandalfwrites@gmail.com>
2022-07-16 14:01:07 -0400

    started email object

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 
 
 let transporter = nodemailer.crearTransport(transport[, defaults])
+let receiver = document.querySelector('#fname').
+
+let response = {
+    from: "sender@email.com",
+    to: 
+}
