d3254f99c587eaa4735be1fd0bd23234fb764e52
Amanda Faulkenberry <amandalfwrites@gmail.com>
2022-07-16 14:38:10 -0400

    fixed transporter

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ document.querySelector('button').addEventListener('click', emailRes)
 let receiver = document.querySelector('#fname').value + document.querySelector('#lname').value
 let receiverEmail = document.querySelector('#email').value
 
-
-let transporter = nodemailer.createTransport(
+    
+    let transporter = nodemailer.createTransport({
+        sendmail: true,
+        newline: 'unix',
+        path: '/usr/sbin/sendmail'
+    })
     
     transport.sendMail({
     from: "sender@email.com",
     to: receiverEmail,
     subject: "Thanks for Reaching Out!",
     text: 
-}, defaults})
+    })
 
